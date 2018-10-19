7b450c703fc0079937739d0bbd900e2d2f23112d
proffessorRick <38213583+proffessorRick@users.noreply.github.com>
2018-10-19 14:46:16 +0200

    Add files via upload

---
+++ code/index.php
@@ -0,0 +1,56 @@
+<?php include "header.php"; ?>
+<main>
+    <h1> GoForWeb </h1>
+    <h2> "For great webdevelopment"</h2>
+    <p> Ons bedrijf staat voor het snel en nauwkeurig leveren <br> van websites voor onze toekomstige
+        opdrachtgevers.<br>
+        Wij streven naar goede communicatie met onze partners <br> en het resultaat naar wens kunnen leveren.
+    </p>
+    <div class="container">
+        <div class="wrapper">
+            <div class="cover">
+                <img class="image" src="img/programmeren.jpg" alt="Te werk gaan" height="98%" width="100%">
+                <article>
+                    <h2>Te werk gaan.</h2>
+                    <br>
+                    <p>Wij bij GoForWeb staan voor snelle en kwalitatieve levering van websites
+                        Wij maken nieuwe wedstrijd en we kunnen ook uw oude website vernieuwen.
+                        Wij zijn een bedrijf met 4 man sterk en zijn allemaal gemotiveerd om uw website naar wens te
+                        leveren.</p>
+                    </ul>
+                </article>
+            </div>
+
+            <div class="wrapper">
+                <div class="cover">
+                    <img class="image" src="img/communiceren.jpg" alt="Communiceren" height="98%" width="100%">
+                    <article>
+                        <h2>Communiceren.</h2>
+                        <br>
+                        <p>Wij als team streven naar goede communicatie met de klant maar ook binnen ons team om snel en
+                            nauwkeurig resultaat te kunnen leveren voor de klant.</p>
+                        </ul>
+                    </article>
+                </div>
+
+                <div class="wrapper">
+                    <div class="cover">
+                        <img class="image" src="img/kwaliteit.jpg" alt="Kwaliteit" height="98%" width="100%">
+                        <article>
+                            <h2>Kwaliteit.</h2>
+                            <br>
+                            <p>Als wij de site geleverd hebben naar wens heeft de klant nog de tijd om de hele site na
+                                te
+                                checken, zo willen wij bereiken dat de site kwalitatief is en compleet naar wens van de
+                                klant. Het kan altijd dat er later nog bugs of errors gevonden worden die zullen wij er
+                                dan
+                                ook uithalen, dat is inbegrepen in de service.</p>
+                            </ul>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include "footer.php"; ?>
