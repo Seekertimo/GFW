115767f72c95f6c4a33541e6d42f5d40cab82ffe
Seekertimo <31450657+Seekertimo@users.noreply.github.com>
2018-11-16 09:35:39 +0100

    Add files via upload

---
+++ project/code/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<article>
+    <form action="">
+        <h3 class="heading">NAW GEGEVENS</h3>
+
+        <h5 class="heading"><u>JE NAAM</u></h5>
+        <label>Voornaam: <br><input type="text" class="input" name="voornaam" value="" placeholder="voornaam"></label><br>
+        <label>Tussenvoegsel: <br><input type="text" class="input" name="tussenvoegsel" value="" placeholder="tussenvoegsel"></label><br>
+        <label>Achternaam: <br><input type="text" class="input" name="achternaam" value="" placeholder="achternaam"></label><br><br>
+
+        <h5 class="heading"><u>JE LOCATIE</u></h5>
+        <label>Postcode: <br><input type="text" class="input" name="postcode" value="" placeholder="postcode"></label><br>
+        <label>Woonplaats: <br><input type="text" class="input" name="woonplaats" value="" placeholder="woonplaats"></label><br><br>
+
+        <h5 class="heading"><u>JE CONTACTGEGEVENS</u></h5>
+        <label>Mobiele nummer: <br><input type="number" class="input" name="mobielnummer" value="" placeholder="mobiele nummer"></label><br>
+        <label>E-mailadres: <br><input type="email" class="input" name="email" value="" placeholder="emailadres"></label><br><br>
+
+        <label><input type="submit"></label>
+    </form>
+</article>
+</body>
+</html>
